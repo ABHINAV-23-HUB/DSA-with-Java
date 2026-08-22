b44bdc89e2cad054c744414e7c923494c4d5f1c2
Abhinav Chaurasia <abhinavchaurasia456@gmail.com>
2026-08-22 21:39:21 +0530

    Time: 3 ms (77.47%) | Memory: 46.3 MB (85.88%) - LeetSync

---
+++ 13-roman-to-integer/roman-to-integer.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int romanToInt(String s) {
+       int result = 0;
+       int len=s.length();
+       for (int i=0 ; i < len; i++){
+        int currval= getValue(s.charAt(i));
+        if ( (i+1 <len) && (currval < getValue(s.charAt(i+1))) ){
+            result-=currval;
+        }
+        else{
+            result+=currval;
+        }
+       } 
+       return result;
+    }
+    private int getValue(char c){
+        switch(c){
+            case 'I': return 1;
+            case 'V': return 5;
+            case 'X': return 10;
+            case 'L': return 50;
+            case 'C': return 100;
+            case 'D': return 500;
+            case 'M' : return 1000;
+            default: return 0;
+
+        }
+    }
+}
